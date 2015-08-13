366d16a4a2382ddd27fbd7d5368dfdbccebd0be5
lyz <702368372@qq.com>
2015-08-13 14:32:02 +0800

    tab->space

--- js/controllers.js
+++ js/controllers.js
@@ -5,18 +5,18 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-    function($scope, Phone) {
-        $scope.phones = Phone.query()
-        $scope.orderProp = 'age';
-    }])
+  function($scope, Phone) {
+    $scope.phones = Phone.query()
+    $scope.orderProp = 'age';
+  }])
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-    function($scope, $routeParams, Phone) {
-        $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-            $scope.mainImageUrl = phone.images[0]
-        })
-        $scope.setImage = function(imageUrl) {
-            $scope.mainImageUrl = imageUrl
-        }
-    }]);
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0]
+    })
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl
+    }
+  }]);
 
