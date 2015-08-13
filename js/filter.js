5e4573b87a432594a34f166f34d7cfc9149af5e3
lyz <702368372@qq.com>
2015-08-13 14:38:34 +0800

    tab->space

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,5 @@
 angular.module('phonecatFilters', []).filter('checkmark', function() {
-	return function(input) {
-		return input ? '\u2713': '\u2718'
-	}
+  return function(input) {
+    return input ? '\u2713': '\u2718'
+  }
 })
